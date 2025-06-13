970a19886c2e4546fd0372536bf73d5c049a2520
Jared Martin <jaredtmartin@gmail.com>
2025-06-13 13:24:14 -0600

    allow uuids of any length

--- uuid.go
+++ uuid.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -11,17 +12,26 @@ const tokenLength = 16
 
 var last_test_uuid = 0
 
+func resetTestUuid() {
+	last_test_uuid = 0
+}
+
 // Uuid generates a random UUID string.
 // In test mode, it generates a sequential UUID for testing purposes.
-func Uuid() string {
+// length is optional and will be rounded down to the nearest even number.
+func Uuid(length ...int) string {
 	env := os.Getenv("ENV")
-
+	l := tokenLength
+	if len(length) > 0 {
+		l = length[0]
+	}
 	if env == "test" {
 		last_test_uuid += 1
-		uuid := fmt.Sprintf("%016d", last_test_uuid)
+		uuid := fmt.Sprintf("%0*d", l, last_test_uuid)
 		return uuid
 	}
-	b := make([]byte, tokenLength)
+	log.Println("Generating UUID in production mode of length", l)
+	b := make([]byte, l/2) // Divide by 2 because hex encoding doubles the length
 	if _, err := rand.Read(b); err != nil {
 		return ""
 	}
--- uuid_test.go
+++ uuid_test.go
@@ -1,17 +1,16 @@
 package bark
 
 import (
+	"log"
 	"os"
 	"testing"
 )
 
 func TestUuidInTestEnv(t *testing.T) {
 	// Set the environment to "test"
+	resetTestUuid()
 	os.Setenv("ENV", "test")
 	defer os.Unsetenv("ENV")
-
-	// Reset the global variable for testing
-	last_test_uuid = 0
 
 	// Generate UUIDs and check their format
 	uuid1 := Uuid()
@@ -32,14 +31,36 @@ func TestUuidInNonTestEnv(t *testing.T) {
 	defer os.Unsetenv("ENV")
 
 	uuid := Uuid()
-
+	log.Println("Generated UUID:", uuid)
 	// Check that the UUID is not empty
 	if uuid == "" {
 		t.Error("Expected a non-empty UUID, got an empty string")
 	}
 
 	// Check that the UUID is of the correct length
-	if len(uuid) != tokenLength*2 {
-		t.Errorf("Expected UUID length to be %d, got %d", tokenLength*2, len(uuid))
+	if len(uuid) != tokenLength {
+		t.Errorf("Expected UUID length to be %d, got %d", tokenLength, len(uuid))
+	}
+}
+func TestUuidWithLength(t *testing.T) {
+	// Set the environment to "test"
+	os.Setenv("ENV", "test")
+	defer os.Unsetenv("ENV")
+	resetTestUuid()
+	testLen := 8
+	uuid := Uuid(testLen)
+	if uuid != "00000001" {
+		t.Errorf("Expected uuid to be '00000001', got '%s'", uuid)
+	}
+	os.Setenv("ENV", "production")
+	uuid = Uuid(testLen)
+	if len(uuid) != testLen {
+		t.Errorf("Expected UUID length to be %d, got %d", testLen, len(uuid))
+	}
+	// Odd lengths will be rounded down
+	testLen = 5
+	uuid = Uuid(testLen)
+	if len(uuid) != testLen-1 {
+		t.Errorf("Expected UUID length to be %d, got %d", testLen-1, len(uuid))
 	}
 }
